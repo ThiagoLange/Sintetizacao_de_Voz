a3693267c3b44d0ff12f42299c7ea36a00b4f335
Thiago Egon Lange <thiago_lange@yahoo.com.br>
2022-06-30 18:24:14 -0300

    Textos em arquivo

---
+++ leitura_arquivo.py
@@ -0,0 +1,12 @@
+import pyttsx3
+
+engine = pyttsx3.init()
+engine.setProperty("voice", "brazil")
+
+arquivo = open("frase.txt", "r", encoding="utf-8")
+conteudo = arquivo.read()
+print(conteudo)
+engine.say(conteudo)
+
+engine.runAndWait()
+arquivo.close()
